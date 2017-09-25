067ab8ee40343e397b2c08129276ecaefdbdc692
robmisasi <Rob.misasi@gmail.com>
2017-09-25 11:36:20 -0500

    fix syntax in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Install dotfiles to home directory
 # if params are given, install those
-if ! [-z $1]; then
-  while ![ -z "$1"]; do
+if ! [ -z "$1" ]; then
+  while ! [ -z "$1" ]; do
     cp "$1" ~
     shift 1
   done
