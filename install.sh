0af88e3f93209fa07413e81c7ab7ee6ad5cf5011
robmisasi <Rob.misasi@gmail.com>
2017-09-26 12:42:02 -0500

    Enhance install.sh to require explicit args to install files, setupLinux.sh modified to better detect vim install

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 # Install dotfiles to home directory
 # if params are given, install those
+if [ -z "$1" ]; then
+  echo "Usage: ./install [-a/filename]"
+  exit
+fi
+
+if [[ $1 == "-a" ]]; then
+  cp .vimrc ~
+  cp .bashrc ~
+  cp .bash_profile ~
+  cp .bash_prompt ~
+  cp .aliases ~
+  exit
+fi
+
 if ! [ -z "$1" ]; then
   while ! [ -z "$1" ]; do
     cp "$1" ~
@@ -8,8 +22,3 @@ if ! [ -z "$1" ]; then
   exit
 fi
 
-cp .vimrc ~
-cp .bashrc ~
-cp .bash_profile ~
-cp .bash_prompt ~
-cp .aliases ~
--- setupLinux.sh
+++ setupLinux.sh
@@ -1,12 +1,15 @@
 # Install useful apps
-if  hash vim 2> /dev/null; then
- sudo apt-get install vim
- mkdir ~/.vim/backups
- mkdir ~/.vim/swaps
+if  command -v vim > /dev/null 2>&1; then
+  echo "Installing vim..."
+  sudo apt-get install vim
+  echo "Making directories..."
+  mkdir ~/.vim/backups
+  mkdir ~/.vim/swaps
 fi
 
 ##  clone vim repos
 # Vundle
+echo "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git/ ~/.vim/bundle/Vundle.vim
 
 # Install vundle plugins int vim with :PluginInstall
